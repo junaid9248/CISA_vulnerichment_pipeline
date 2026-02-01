28bbecb8398faccaeb733f4f8081720a4be61852
Mohammed Junaid <junaidwork456@gmail.com>
2026-02-01 00:39:09 -0600

    fixed paths in scripts

--- scripts/create_combined.sh
+++ scripts/create_combined.sh
@@ -1,2 +1,4 @@
-head -n 1 /dataset/downloaded_artifacts/cve_2001/cve_2001.csv >> /dataset/cve_combined.csv          
-tail -n +2 -q /dataset/downloaded_artifacts/cve_*/cve_*.csv >> /dataset/cve_combined.csv
+#!/bin/bash
+
+head -n 1 ../dataset/downloaded_artifacts/cve_2001/cve_2001.csv >> ../dataset/cve_combined.csv
+tail -n +2 -q ../dataset/downloaded_artifacts/cve_*/cve_*.csv >> ../dataset/cve_combined.csv
--- scripts/kaggle_credentials.sh
+++ scripts/kaggle_credentials.sh
@@ -1,5 +1,7 @@
-
+cd ..
 mkdir -p ~/.kaggle
 echo {"name": "$KAGGLE_USERNAME", "key": "$KAGGLE_KEY"} >>  ~/.kaggle/kaggle.json
 #Setting to r+w 
 chmod 600 ~/.kaggle/kaggle.json
+
+cd scripts
